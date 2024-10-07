884bff5945a9fa800066180b006a7b4bcd072a38
userpbakker <pb17301@gmail.com>
2024-10-07 21:23:48 +0200

    mesh alignment

--- radioCommunication/mesh_nano1/mesh_nano1.ino
+++ radioCommunication/mesh_nano1/mesh_nano1.ino
@@ -25,94 +25,6 @@ RF24 radio(10, 9);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
  
-/*
- * User Configuration: nodeID - A unique identifier for each radio. Allows addressing
- * to change dynamically with physical changes to the mesh.
- *
- * In this example, configuration takes place below, prior to uploading the sketch to the device
- * A unique value from 1-255 must be configured for each node.
- */
- /*
-#define nodeID 5
- 
-uint32_t displayTimer = 0;
- 
-struct payload_t {
-  unsigned long ms;
-  unsigned long counter;
-};
- 
-/*
-void setup() {
-  Serial.begin(115200);
- 
-  // Set the nodeID manually
-  mesh.setNodeID(nodeID);
- 
-  // Set the PA Level to MIN and disable LNA for testing & power supply related issues
-  radio.begin();
-  radio.setPALevel(RF24_PA_MIN, 0);
- 
-  // Connect to the mesh
-  Serial.println("Connecting to the mesh...");
-  if (!mesh.begin()) {
-    if (radio.isChipConnected()) {
-      do {
-        // mesh.renewAddress() will return MESH_DEFAULT_ADDRESS on failure to connect
-        Serial.println("Could not connect to network.\nConnecting to the mesh...");
-      } while (mesh.renewAddress() == MESH_DEFAULT_ADDRESS);
-    } else {
-      Serial.println("Radio hardware not responding.");
-      while (1) {
-        // hold in an infinite loop
-      }
-    }
-  }
-}
- 
-void loop() {
- 
-  mesh.update();
- 
-  // Send to the master node every second
-  if (millis() - displayTimer >= 1000) {
-    displayTimer = millis();
- 
-    // Send an 'M' type message containing the current millis()
-    if (!mesh.write(&displayTimer, 'M', sizeof(displayTimer))) {
- 
-      // If a write fails, check connectivity to the mesh network
-      if (!mesh.checkConnection()) {
-        //refresh the network address
-        Serial.println("Renewing Address");
-        if (mesh.renewAddress() == MESH_DEFAULT_ADDRESS) {
-          //If address renewal fails, reconfigure the radio and restart the mesh
-          //This allows recovery from most if not all radio errors
-          mesh.begin();
-        }
-      } else {
-        Serial.println("Send fail, Test OK");
-      }
-    } else {
-      Serial.print("Send OK: ");
-      Serial.println(displayTimer);
-    }
-  }
- 
-  /*
-  while (network.available()) {
-    RF24NetworkHeader header;
-    payload_t payload;
-    network.read(header, &payload, sizeof(payload));
-    Serial.print("Received packet #");
-    Serial.print(payload.counter);
-    Serial.print(" at ");
-    Serial.println(payload.ms);
-  } 
-  
-}
-/* */
- 
 // Payload to MASTER
 struct payload_from_master {
   unsigned long counter;
@@ -154,9 +66,9 @@ void setup() {
 }
  
 void loop() {
+  // Call mesh.update to keep the network updated
   mesh.update();
  
-  /* */
   //// Send to the master node every two seconds - BEGIN
   if (millis() - sleepTimer > 2000) {
     sleepTimer = millis();
@@ -177,7 +89,6 @@ void loop() {
       Serial.println(payload.timer);
     }
   }
-  /* */
   //// Send to the master node every two seconds - END
  
   //// Receive a message from master if available - START
@@ -191,6 +102,7 @@ void loop() {
     Serial.println(payload.showLed);
  
     /*
+    // this LED is not connected, this example uses the serial output to confirm the mesh is working
     showLed = payload.showLed;
  
     if (payload.showLed) {
--- radioCommunication/mesh_uno1_with_RFmodule/mesh_uno1_with_RFmodule.ino
+++ radioCommunication/mesh_uno1_with_RFmodule/mesh_uno1_with_RFmodule.ino
@@ -23,102 +23,10 @@ this one:
 https://forum.arduino.cc/t/simple-nrf24l01-2-4ghz-transceiver-demo/405123/3
 
 */
-RF24 radio(9, 10);
+RF24 radio(10, 9);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
  
-/*
- * User Configuration: nodeID - A unique identifier for each radio. Allows addressing
- * to change dynamically with physical changes to the mesh.
- *
- * In this example, configuration takes place below, prior to uploading the sketch to the device
- * A unique value from 1-255 must be configured for each node.
- */
-/*
-#define nodeID 0
- 
-uint32_t displayTimer = 0;
- 
-struct payload_t {
-  unsigned long ms;
-  unsigned long counter;
-};
- 
- /*
-void setup() {
-  Serial.begin(115200);
-  while (!Serial) {
-    // some boards need this because of native USB capability
-  }
- 
-  // Set the nodeID manually
-  mesh.setNodeID(nodeID);
- 
-  // Set the PA Level to MIN and disable LNA for testing & power supply related issues
-  radio.begin();
-  radio.setPALevel(RF24_PA_MIN, 0);
- 
-  // Connect to the mesh
-  Serial.println("Connecting to the mesh...");
-  if (!mesh.begin()) {
-    if (radio.isChipConnected()) {
-      do {
-        // mesh.renewAddress() will return MESH_DEFAULT_ADDRESS on failure to connect
-        Serial.println("Could not connect to network.\nConnecting to the mesh...");
-      } while (mesh.renewAddress() == MESH_DEFAULT_ADDRESS);
-    } else {
-      Serial.println("Radio hardware not responding.");
-      while (1) {
-        // hold in an infinite loop
-      }
-    }
-  }
-}
- 
-void loop() {
- 
-  mesh.update();
- 
-  // Send to the master node every second
-  /* this UNO is the master
-  if (millis() - displayTimer >= 1000) {
-    displayTimer = millis();
- 
-    // Send an 'M' type message containing the current millis()
-    if (!mesh.write(&displayTimer, 'M', sizeof(displayTimer))) {
- 
-      // If a write fails, check connectivity to the mesh network
-      if (!mesh.checkConnection()) {
-        //refresh the network address
-        Serial.println("Renewing Address");
-        if (mesh.renewAddress() == MESH_DEFAULT_ADDRESS) {
-          //If address renewal fails, reconfigure the radio and restart the mesh
-          //This allows recovery from most if not all radio errors
-          mesh.begin();
-        }
-      } else {
-        Serial.println("Send fail, Test OK");
-      }
-    } else {
-      Serial.print("Send OK: ");
-      Serial.println(displayTimer);
-    }
-  }
- 
-  while (network.available()) {
-    RF24NetworkHeader header;
-    payload_t payload;
-    network.read(header, &payload, sizeof(payload));
-    Serial.print("Received packet #");
-    Serial.print(payload.counter);
-    Serial.print(" at ");
-    Serial.println(payload.ms);
-  }
-}
-/* */
-
-
-
 // Payload from MASTER
 struct payload_from_master {
   unsigned long counter;
@@ -200,7 +108,7 @@ void loop() {
       for(int i=0; i<mesh.addrListTop; i++){
         Serial.print(F("NodeID: "));
         Serial.print(mesh.addrList[i].nodeID);
-        Serial.print(F(" RF24Network Address: 0"));
+        Serial.print(F(" RF24Network Address: 0")); // this is in octal
         Serial.println(mesh.addrList[i].address,OCT);
       }
       Serial.println(F("**********************************"));
