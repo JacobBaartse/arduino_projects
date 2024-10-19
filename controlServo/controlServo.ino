48708737a499071a4712c1bd714e0515b44f5a96
userpbakker <pb17301@gmail.com>
2024-10-19 17:03:10 +0200

    added comments for motor controller

--- controlServo/controlServo.ino
+++ controlServo/controlServo.ino
@@ -1,11 +1,11 @@
 /* 
  *  This servo, SG90 360 degrees seems to be a motor:
  *
- *  position   0 means maximum speed left
- *  position  45 means medium speed left
+ *  position   0 means maximum speed right (clockwise)
+ *  position  45 means medium speed right (clockwise)
  *  position  90 means still, not moving, idle position
- *  position 135 means medium speed right
- *  position 180 means maximum speed right
+ *  position 135 means medium speed left (counterclockwise)
+ *  position 180 means maximum speed left (counterclockwise)
  */
 
 #include <Servo.h>
@@ -17,16 +17,19 @@ int pos = 0;    // variable to store the servo position
 
 
 void set_servo(int position, int duration=1000) {
-  Serial.print(F("Servo to position: ")); 
+  Serial.print(F("Servo position/speed: ")); 
   Serial.println(position);
 
   delay(duration);
   myservo.write(position); 
 
-  Serial.print(F("Servo in position: ")); 
+  Serial.print(F("Servo position/speed: ")); 
   Serial.println(position);
   delay(duration);
 }
+
+int valinput = 90; // idle for this servo
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) {
@@ -34,9 +37,9 @@ void setup() {
   }
 
   myservo.attach(9); // attaches the servo on pin 9 to the Servo object
-}
 
-int valinput = 0;
+  set_servo(valinput);
+}
 
 void loop() {
   // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
