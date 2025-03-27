bbd68078f0229e7782d159e7df8d3368ca28edea
userpbakker <pb17301@gmail.com>
2025-03-27 21:26:22 +0100

    prepare next sketches

--- DisplayUno2/networkdata.h
+++ DisplayUno2/networkdata.h
@@ -1,15 +1,10 @@
+/*
+ * 
+ */
 
 String PROGMEM knownnetworks[][3] = {
-  { "T24_optout", "T24T24T24", "SO_148" }, // thuis
-  //{"liantegastwifi", "Welkom@Liante", "Liante"}, // Liante
-  { "K24", "K24K24K24", "Kantoor" }, // kantoor
-  { "Tonny_optout", "Hermeler1234", "GVV_17301" }, // 1e in Hoofddorp
-  //{ "Wifi2.4G", "KeesG2.4", "GVV_17301" }, // 2e in Hoofddorp
-  { "Koos4G_optout", "piet1234", "onderweg1"}, // onderweg 1
-  { "Kees4G_optout", "piet1234", "onderweg2"}, // onderweg 2
-  { "PBonderweg", "a1b2c3d4", "onderweg3"}, // onderweg 3
-  { "Ziggo7F331C7", "kw4mamEzn7ah", "JWF_21"}, // Spoorstra
-  
+  { "SSID_1", "PASS_1", "Location_1" }, 
+  { "SSID_2", "PASS_2", "Location_2" }, 
+  { "SSID_3", "PASS_3", "Location_3" }, 
   { "EOR", "-", "-"} // End Of Array indication
 }; 
-
