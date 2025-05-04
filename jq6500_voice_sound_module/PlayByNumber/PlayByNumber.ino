035b3ffe37e9bce25447524391eb0b5bc97721a3
jacob Baartse <j.baartse@gmail.com>
2025-05-04 19:14:49 +0200

    example added for jq6500 mp3 player

---
+++ jq6500_voice_sound_module/PlayByNumber/PlayByNumber.ino
@@ -0,0 +1,35 @@
+/** Demonstrate how to play a file by it's (FAT table) index number.
+ *
+ * @author James Sleeman,  http://sparks.gogo.co.nz/
+ * @license MIT License
+ * @file
+ */
+ 
+#include <Arduino.h>
+#include <JQ6500_Serial.h>
+// https://github.com/sleemanj/JQ6500_Serial  download zip and install library
+
+// Create the mp3 module object. In this example we're using the Hardware Serial port, so:
+// Arduino Pin RX d0 is connected to TX of the JQ6500
+// Arduino Pin TX d1 is connected to one end of a  1k resistor, 
+// the other end of the 1k resistor is connected to RX of the JQ6500
+// If your Arduino is 3v3 powered, you can omit the 1k series resistor
+JQ6500_Serial mp3(Serial1);  // for arduino R4 wifi Serial1 has to be used. while connected to d0, d1
+
+void setup() {
+  Serial1.begin(9600);
+  mp3.reset();
+  mp3.setVolume(20);
+  mp3.setLoopMode(MP3_LOOP_NONE);
+
+}
+
+void loop() {
+  for (int i=0; i<12;){
+    if(mp3.getStatus() != MP3_STATUS_PLAYING)
+    {
+      i++;
+      mp3.playFileByIndexNumber(i);  
+    } 
+  }
+}
