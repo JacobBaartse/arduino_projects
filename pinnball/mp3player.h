8bd2f636356b602d434bd268a1e9de143f4533e7
jacob Baartse <j.baartse@gmail.com>
2025-05-11 21:25:02 +0200

    mp3 player added buzzer disabled.

---
+++ pinnball/mp3player.h
@@ -0,0 +1,94 @@
+/** Demonstrate how to play a file by it's (FAT table) index number.
+ *
+
+
+ * @author James Sleeman,  http://sparks.gogo.co.nz/
+ * @license MIT License
+ * @file
+ */
+ 
+// #include <Arduino.h>
+// #include <JQ6500_Serial.h>
+// https://github.com/sleemanj/JQ6500_Serial  download zip and install library
+
+// Create the mp3 module object. In this example we're using the Hardware Serial port, so:
+// Arduino Pin RX d0 is connected to TX of the JQ6500
+// Arduino Pin TX d1 is connected to one end of a  1k resistor, 
+// the other end of the 1k resistor is connected to RX of the JQ6500
+// If your Arduino is 3v3 powered, you can omit the 1k series resistor
+JQ6500_Serial mp3(Serial1);  // for arduino R4 , R4 wifi Serial1 has to be used. while connected to d0, d1
+
+// int num_files = 0;
+// int i=0;
+
+const int CANNON_SHOT = 0;
+const int DO_RE_MI = 1;
+const int FIEEEW = 2;
+const int GUN_SHOT = 3;
+const int HARP_TRIANGEL = 4;
+const int HART_SLAG = 5;
+const int HIGH_ANOTHERONE = 6;
+const int HIGH_PING = 7;
+const int INTRO_MELODY = 8;
+const int JAMMER = 9;
+const int JUICHEN = 10;
+const int KIP = 11;
+const int KLIK_KLAK = 12;
+const int KLOK_TIKKEN = 13;
+const int KLONGNGNG = 14;
+const int KOEKOEK_KLOK = 15;
+const int KREKEL = 16;
+const int LOW_ANOTHERONE = 17;
+const int OLD_TELEPHONE_RING = 18;
+const int P_OEIJJ = 19;
+const int PAARD = 20;
+const int POING_5X = 21;
+const int POINOININ = 22;
+const int PRRRR = 23;
+const int SUPER_GOOD = 24;
+const int SUPER_SCORE = 25;
+const int TADI_TADIADI = 26;
+const int TATA_TSJING = 27;
+const int TIE_WIEWIE = 28;
+const int TING_TING_TING = 29;
+const int TOE_OE_3X = 30;
+const int TOE_TOKKK = 31;
+const int TONG_TING_HIGH = 32;
+const int TRRRRR = 33;
+const int WOOOWOWOW = 34;
+const int XILOFOON_UP = 35;
+
+
+
+void setup_mp3_player() {
+  Serial1.begin(9600);
+  mp3.reset();
+  mp3.setVolume(30);
+  mp3.setLoopMode(MP3_LOOP_NONE);
+  // num_files = mp3.countFiles(MP3_SRC_BUILTIN);
+}
+
+void Play_mp3_file(byte songNumber) {
+  byte command[] = {0x7E, 0x04, 0x03, 0x00, songNumber+1, 0xEF};
+  Serial1.write(command, sizeof(command));
+  // mp3.playFileByIndexNumber(songNumber+1);  
+}
+
+void sendCommand(byte command[], int length) {
+  while (Serial1.available()) Serial1.read();
+  Serial1.write(command, length);
+}
+
+void playTrack( byte b){
+  byte command[] = {0x7E, 0x04, 0x03, 0x00, b+1, 0xEF};
+  Serial1.write(command, sizeof(command));
+  // int count = 0;
+  // while (Serial1.available() == 0  && count < 2){
+  //   count++;
+  //   delay(100);
+  // }
+  // while (Serial1.available()) {
+  //   byte b = Serial1.read();
+  // }
+}
+
--- pinnball/pinnball.ino
+++ pinnball/pinnball.ino
@@ -3,8 +3,8 @@
 // #include <Servo.h>
 // #include "my_servo.h"
 
-#include <CuteBuzzerSounds.h>
-#include "my_buzzer.h"
+// #include <CuteBuzzerSounds.h>
+// #include "my_buzzer.h"
 
 #include "my_io_extender.h"
 
@@ -15,6 +15,10 @@
 #include <FastLED.h>
 #include "my_led_strip.h"
 
+#include <Arduino.h>
+#include <JQ6500_Serial.h>
+#include "mp3player.h"
+
 uint32_t score_counter = 0;
 
 void setup()
@@ -22,11 +26,13 @@ void setup()
   Serial.begin(115200);
   setup_io_extender();
   // setup_servo();
-  buzzer_setup();
+  // buzzer_setup();
   display_setup();
   lcd.print(score_counter, 0);
   ledstrip_setup();
   show_leds_rainbow();
+  setup_mp3_player();
+  Play_mp3_file(INTRO_MELODY);
 }
 
 bool left1hit = false;
@@ -43,60 +49,60 @@ void loop()
 {
   int switch_nr = io_extender_check_switches();
   if (switch_nr == 1){
+    Play_mp3_file(FIEEEW);
     show_leds_reandom(1, false);
-    cute_buzzer(1);
     score_counter += 10;
     show_leds_reandom(10, true);
   }
   if (switch_nr == 2){
+    Play_mp3_file(DO_RE_MI);
     show_leds_rainbow();
-    cute_buzzer(2);
     score_counter += 100;
     delay(300);
     show_leds_reandom(0, true);
   }
   if (switch_nr == 3){
+    Play_mp3_file(CANNON_SHOT);
     show_leds_reandom(1, false);
-    cute_buzzer(3);
     score_counter += 100;
     show_leds_reandom(10, true);
   }  
   if (switch_nr == 10){  //red button
+    Play_mp3_file(GUN_SHOT);
     reset_lefthit();
-    cute_buzzer(4);
     score_counter = 0;
   }  
   if (switch_nr == 9){  //green button
-    cute_buzzer(5);
+    Play_mp3_file(KOEKOEK_KLOK);
     //score_counter = 0;
   }
   if (switch_nr == 11){  //left side button 1
     left1hit = true;
+    Play_mp3_file(PRRRR);
     score_onleds(left1hit, left2hit, left3hit);
-    cute_buzzer(6);
-    blink_leds(left1hit, false, false);
+    blink_leds(left1hit, false, false, 2000);
     score_counter += 5;
   }
   if (switch_nr == 12){  //left side button 2
     left2hit = true;
+    Play_mp3_file(HIGH_PING);
     score_onleds(left1hit, left2hit, left3hit);
-    cute_buzzer(7);
-    blink_leds(false, left2hit, false);
+    blink_leds(false, left2hit, false, 1400);
     score_counter += 5;
   }
   if (switch_nr == 13){  //left side button 3
     left3hit = true;
+    Play_mp3_file(OLD_TELEPHONE_RING);
     score_onleds(left1hit, left2hit, left3hit);
-    cute_buzzer(8);
-    blink_leds(false, false, left3hit);
+    blink_leds(false, false, left3hit, 5000);
     score_counter += 5;
   }
 
   if (left1hit & left2hit & left3hit){
+    Play_mp3_file(SUPER_GOOD);
     score_onleds(left1hit, left2hit, left3hit);
     score_counter += 100;
-    blink_leds(left1hit, left2hit, left3hit);
-    cute_buzzer(9);
+    blink_leds(left1hit, left2hit, left3hit, 2000);
     reset_lefthit();
   }
 
