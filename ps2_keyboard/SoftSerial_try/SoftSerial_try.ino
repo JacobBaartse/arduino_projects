2acb46e84b7214f667a2265a02bfd6c07a968c27
jacob Baartse <j.baartse@gmail.com>
2025-06-21 06:33:28 +0200

    little update to ps2 keyboard examples

--- ps2_keyboard/SoftSerial_try/SoftSerial_try.ino
+++ ps2_keyboard/SoftSerial_try/SoftSerial_try.ino
@@ -2,9 +2,10 @@
 
 #define rxPin 2
 #define txPin 2 // for this keyboard application only the rx pin is used, 
+#define buffer_size 32
 
 // Set up a new SoftwareSerial object
-SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin, buffer_size);
 
 void setup()  {
   Serial.begin(115200);
@@ -16,17 +17,17 @@ void setup()  {
     pinMode(txPin, OUTPUT);
     
     // Set the baud rate for the SoftwareSerial object
-    mySerial.begin(12600, SERIAL_8O1);  // depending on the keyboad the boudrate should be set to somting between 10000 and 17000 bits per second.
+    mySerial.begin(12600, SERIAL_8O1);  // depending on the keyboad the baud rate should be set to something between 10000 and 17000 bits per second.
 }
 
 void loop() {
     if (mySerial.available() > 0) {
         int mychar = mySerial.read();
         // mychar &= 0xff;
-        if (mychar != 0xf0) 
-          if 
-            (mychar == 0xe0) Serial.print(mychar, HEX);
-          else  
+        // if (mychar != 0xf0) 
+        //   if 
+        //     (mychar == 0xe0) Serial.print(mychar, HEX);
+        //   else  
             Serial.println(mychar, HEX);
     }
 }
--- ps2_keyboard/keyboardToScreen/ps2_keyboard.h
+++ ps2_keyboard/keyboardToScreen/ps2_keyboard.h
@@ -1,31 +1,35 @@
-// #include <SoftwareSerial.h>
+//Works with HP fullsize ps2 keyboard.
 
+
+
+// Set up a new SoftwareSerial object uncomment next 5 lines.
+#include <SoftwareSerial.h>
 #define rxPin 2
-#define txPin 2 // for this keyboard application only the rx pin is used, 
+#define txPin 2 // for this keyboard application only the rx pin is used,   for arduino r4 ony pin 2 or pin 3 (which have interrupts) can be used.
+#define buffer_size 32
+SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin, buffer_size);  // softwareserial could use any pin
 
-// Set up a new SoftwareSerial object
-SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+// if available also the hardware uart on D0 could be used uncomment next line.
+// HardwareSerial &mySerial = Serial1; // if available also the hardware uart on D0 could be used
 
 const int len_arr = 49;
 const int conversion_table_int[] =       {0x1c,0x32,0x21,0x23,0x24,0x2b,0x34,0x33,0x43,0x3b,0x42,0x4b,0x3a,0x31,0x44,0x4d,0x15,0x2d,0x1b,0x2c,0x3c,0x2a,0x1d,0x22,0x35,0x1a,0x16,0x1e,0x26,0x25,0x2e,0x36,0x3d,0x3e,0x46,0x45,0x4e,0x55,0x29,0x54,0x5b,0x5d,0x4c,0x52,0x41,0x49,0x4a,0x66,0x5a};
 const char conversion_table_asc[] =       {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', ' ', '[', ']','\\', ';','\'', ',', '.', '/','\b','\n'};
 const char conversion_table_asc_shift[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', ' ', '{', '}', '|', ':', '"', '<', '>', '?','\b','\n'};
 bool shift = false;
-bool ps2_debug = false;
+bool ps2_debug = true;
 
 void setup_ps2_keyboard()  {
-    // Define pin modes for TX and RX
-    pinMode(rxPin, INPUT);
-    pinMode(txPin, OUTPUT);
-    
     // Set the baud rate for the SoftwareSerial object
-    mySerial.begin(12600, SERIAL_8O1);  // depending on the keyboad the boudrate should be set to somting between 10000 and 17000 bits per second.
+    mySerial.begin(12600, SERIAL_8O1);  // depending on the keyboad the baud rate should be set to something between 10000 and 16600 bits per second.
+                                        // check with the trace sketch to find the baud rate.
 }
 
 
 int read_keyboard(){
   if (mySerial.available() > 0) {
     int mychar = mySerial.read();
+    Serial.println(mychar, HEX);
     // return mychar1;
     if (mychar == 0xe0){
       delay(4);
