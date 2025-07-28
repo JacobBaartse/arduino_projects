ff54243afe8a05eb1f7e0543712925d44dcf2590
jacob Baartse <j.baartse@gmail.com>
2025-07-28 15:04:05 +0200

    different pins color screen test

--- colorDisplay_ili9341/graphicstest/graphicstest.ino
+++ colorDisplay_ili9341/graphicstest/graphicstest.ino
@@ -20,8 +20,8 @@
 
 // For the Adafruit shield, these are the default.
 #define TFT_DC 9
-#define TFT_CS 10
-#define TFT_RST 8
+#define TFT_CS 6 // 10
+#define TFT_RST 5 // 8
 #define TFT_MOSI 11
 #define TFT_CLK 13
 #define TFT_MISO 12
@@ -131,8 +131,8 @@ unsigned long testText() {
   tft.fillScreen(ILI9341_BLACK);
   unsigned long start = micros();
   tft.setCursor(0, 0);
-  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
-  tft.println("Hello World!");
+  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(3);
+  tft.println("abcdefghijklm\n\nnopqrstuvwxyz\n\nABCDEFGHIJKLM\n\nNOPQRSTUVWXYZ");
   tft.setTextColor(ILI9341_YELLOW); tft.setTextSize(2);
   tft.println(1234.56);
   tft.setTextColor(ILI9341_RED);    tft.setTextSize(3);
