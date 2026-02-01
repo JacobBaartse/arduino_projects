6c3eaa7d7665a511479020be75c9192ede62b7d3
userpbakker <pb17301@gmail.com>
2026-02-01 17:07:05 +0100

    preparation for ESP trial

---
+++ ESPstart/ESPstart.ino
@@ -0,0 +1,17 @@
+
+
+
+void setup() {
+  delay(1000);
+  Serial.println(F("Starting ESP device"));
+  Serial.flush();
+  Serial.print(__FILE__);
+  Serial.print(F(", creation/build time: "));
+  Serial.println(__TIMESTAMP__);
+  Serial.flush(); 
+  
+}
+
+void loop() {
+
+}
