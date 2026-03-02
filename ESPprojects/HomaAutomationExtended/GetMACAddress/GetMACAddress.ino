ec269c4a25431d04f331594cb51f6731b75f5402
userpbakker <pb17301@gmail.com>
2026-03-02 14:00:37 +0100

    ESP add LED flash function

--- ESPprojects/HomaAutomationExtended/GetMACAddress/GetMACAddress.ino
+++ ESPprojects/HomaAutomationExtended/GetMACAddress/GetMACAddress.ino
@@ -4,10 +4,18 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println();
-  Serial.print("MAC address: ");
+  // Ensure WiFi is initialized
+  WiFi.mode(WIFI_AP_STA);
+
+  // Get Station MAC
+  Serial.print("Station MAC: ");
   Serial.println(WiFi.macAddress());
+
+  // Get SoftAP MAC
+  Serial.print("SoftAP MAC: ");
+  Serial.println(WiFi.softAPmacAddress());
 }
 
 void loop() {
+  
 }
